63210fa3639347c43a440b70be2e4d56075c985d
gautierenaud <gautierenaud@gmail.com>
2016-03-09 18:09:08 +0100

    finished implementing labelTable

--- tableLabels.c
+++ tableLabels.c
@@ -1,11 +1,53 @@
 #include "tableLabels.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void initLabelTable(tableLabels *table){
     table->size = 0;
     table->capacity = LABEL_CAPACITY;
+    table->labels = malloc(sizeof(int*) * table->capacity);
 }
 
-void resizeInstructionTable(tableLabels *table){
+void resizeLabelTable(tableLabels *table){
     table->capacity *= 2;
     table->labels = (int**) realloc(table->labels, table->capacity * sizeof(*table->labels));   
+}
+
+void freeLabelTable(tableLabels *table){
+    int i;
+    for (i = 0; i < table->size; i++){
+        free(table->labels[i]);
+    }
+    free(table->labels);
+    free(table);
+}
+
+void printLabelTable(tableLabels *table){
+    printf("size of label table: %d; capacity: %d\n", table->size, table->capacity);
+    int i;
+    for (i = 0; i < table->size; i++){
+        printf("\tfrom: %d; to: %d\n", table->labels[i][0], table->labels[i][1]);
+    }
+}
+
+int addLabel1(tableLabels *table, int from){
+    if (table->size == table->capacity)
+        resizeLabelTable(table);
+    table->labels[table->size] = malloc(sizeof(int) * 2);
+    table->labels[table->size][0] = from;
+    table->labels[table->size][1] = -1;
+    return table->size++;
+}
+
+int addLabel2(tableLabels *table, int from, int to){
+    if (table->size == table->capacity)
+        resizeLabelTable(table);
+    table->labels[table->size] = malloc(sizeof(int) * 2);
+    table->labels[table->size][0] = from;
+    table->labels[table->size][1] = to;
+    return table->size++;
+}
+
+void updateLabel(tableLabels *table, int index, int to){
+    table->labels[index][1] = to;
 }
--- tableLabels.h
+++ tableLabels.h
@@ -4,7 +4,7 @@
 #define LABEL_CAPACITY 200 
 
 typedef struct{
-    int labels[LABEL_CAPACITY][2];
+    int **labels;
     int size;
     int capacity;
 }tableLabels;
@@ -13,5 +13,12 @@ void initLabelTable(tableLabels *table);
 void freeLabelTable(tableLabels *table);
 void resizeLabelTable(tableLabels *table);
 void printLabelTable(tableLabels *table);
+
+// add method for if jumps
+int addLabel1(tableLabels *table, int from);
+// to update the destination after the if
+void updateLabel(tableLabels *table, int index, int to);
+// add method for other jumps
+int addLabel2(tableLabels *table, int from, int to);
 
 #endif
