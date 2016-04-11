d43ff115b36d088b4619dfbda3c3d54a479325ca
PaulNeissen <neissen@etud.isa-toulouse.fr>
2016-04-11 10:46:54 +0200

    processeur

--- Archi_Info/Processeur.vhd
+++ Archi_Info/Processeur.vhd
@@ -48,8 +48,6 @@ architecture Behavioral of Processeur is
 		  );
 	 END COMPONENT;
 	 
-	 
-	 
 	 COMPONENT Mem_Instructions
 		 PORT(
 			CLK : IN std_logic;
@@ -68,13 +66,151 @@ architecture Behavioral of Processeur is
 			dout : OUT  std_logic_vector(7 downto 0)
 		  );
 	 END COMPONENT;
+	 
+	 COMPONENT FourReg PORT (
+			  IA : in  STD_LOGIC_VECTOR (7 downto 0);
+           IOP : in  STD_LOGIC_VECTOR (7 downto 0);
+           IB : in  STD_LOGIC_VECTOR (7 downto 0);
+           IC : in  STD_LOGIC_VECTOR (7 downto 0);
+           OA : out  STD_LOGIC_VECTOR (7 downto 0);
+           OOP : out  STD_LOGIC_VECTOR (7 downto 0);
+           OB : out  STD_LOGIC_VECTOR (7 downto 0);
+           OC : out  STD_LOGIC_VECTOR (7 downto 0);
+           CLK : in  STD_LOGIC;
+			  RST : in STD_LOGIC);
+	END COMPONENT;
+	
+		 COMPONENT ThreeReg PORT (
+			  IA : in  STD_LOGIC_VECTOR (7 downto 0);
+           IOP : in  STD_LOGIC_VECTOR (7 downto 0);
+           IB : in  STD_LOGIC_VECTOR (7 downto 0);
+           OA : out  STD_LOGIC_VECTOR (7 downto 0);
+           OOP : out  STD_LOGIC_VECTOR (7 downto 0);
+           OB : out  STD_LOGIC_VECTOR (7 downto 0);
+           CLK : in  STD_LOGIC;
+			  RST : in STD_LOGIC);
+	END COMPONENT;
+	
+	COMPONENT LogicCombi PORT (
+           DIN : in  STD_LOGIC_VECTOR (7 downto 0);
+			  CLK : in STD_LOGIC;
+           DOUT : out  STD_LOGIC);
+	END COMPONENT;
+			
+signal CK : std_logic := '0';
+signal memInstructOut : std_logic_vector (31 downto 0) := (others => '0');
+signal A0 : std_logic_vector (7 downto 0) := (others => '0');
+signal A1 : std_logic_vector (7 downto 0) := (others => '0');
+signal A2 : std_logic_vector (7 downto 0) := (others => '0');
+signal A3 : std_logic_vector (7 downto 0) := (others => '0');
+signal A4 : std_logic_vector (7 downto 0) := (others => '0');
+signal OP0 : std_logic_vector (7 downto 0) := (others => '0');
+signal OP1 : std_logic_vector (7 downto 0) := (others => '0');
+signal OP2 : std_logic_vector (7 downto 0) := (others => '0');
+signal OP3 : std_logic_vector (7 downto 0) := (others => '0');
+signal OP4 : std_logic_vector (7 downto 0) := (others => '0');
+signal B0 : std_logic_vector (7 downto 0) := (others => '0');
+signal B1 : std_logic_vector (7 downto 0) := (others => '0');
+signal B2 : std_logic_vector (7 downto 0) := (others => '0');
+signal B3 : std_logic_vector (7 downto 0) := (others => '0');
+signal B4 : std_logic_vector (7 downto 0) := (others => '0');
+signal C0 : std_logic_vector (7 downto 0) := (others => '0');
+signal C1 : std_logic_vector (7 downto 0) := (others => '0');
+signal C2 : std_logic_vector (7 downto 0) := (others => '0');
+signal C3 : std_logic_vector (7 downto 0) := (others => '0');
+signal C4 : std_logic_vector (7 downto 0) := (others => '0');
+signal IP : std_logic_vector (15 downto 0) := (others => '0');
+signal AdrA : std_logic_vector (7 downto 0) := (others => '0');
+signal AdrB : std_logic_vector (7 downto 0) := (others => '0');
+signal QA : std_logic_vector (7 downto 0) := (others => '0');
+signal QB : std_logic_vector (7 downto 0) := (others => '0');
+signal WBR : std_logic := '0';
+
 
 begin
 
-process
-	begin
-		wait until ck'event and ck='1';
-end process;
+memInstruct : Mem_Instructions PORT MAP (
+			CLK => CLK,
+			Address => IP,     
+			Dout => memInstructOut
+			);
+			
+LIDI : FourReg PORT MAP (
+			IA => memInstructOut(23 downto 16),
+         IOP => memInstructOut(31 downto 24),
+         IB => memInstructOut(15 downto 8),
+         IC => memInstructOut(7 downto 0),
+         OA => A1,
+         OOP => OP1,
+         OB => B1,
+         OC => C1,
+         CLK => CLK,
+			RST => RST
+			);
+			
+DIEX : FourReg PORT MAP (
+			IA => A1,
+         IOP => OP1,
+         IB => B1,
+         IC => C1,
+         OA => A2,
+         OOP => OP2,
+         OB => B2,
+         OC => C2,
+         CLK => CLK,
+			RST => RST
+			);
+			
+EXMem : ThreeReg PORT MAP (
+			IA => A2,
+         IOP => OP2,
+         IB => B2,
+         OA => A3,
+         OOP => OP3,
+         OB => B3,
+         CLK => CLK,
+			RST => RST
+			);
+			
+MemRE : ThreeReg PORT MAP (
+			IA => A3,
+         IOP => OP3,
+         IB => B3,
+         OA => A4,
+         OOP => OP4,
+         OB => B4,
+         CLK => CLK,
+			RST => RST
+			);
+
+br : banc_registres PORT MAP (
+			 AddrA => AdrA, 
+          AddrB => AdrB,
+          AddrW => A4,
+          W => WBR,
+          DATA => B4,
+          RST => RST,
+          CLK => CLK,
+          QA => QA,
+          QB => QB
+);
+
+LC : LogicCombi PORT MAP (
+           DIN => OP4,
+			  CLK => CLK,
+           DOUT => WBR
+			  );
+
+
+
+
+SA <= QA;
+SB <= QB;
+
+--process
+--	begin
+--		wait until ck'event and ck='1';
+--end process;
 
 end Behavioral;
 
---
+++ Archi_Info/test_Processeur.vhd
@@ -0,0 +1,70 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY test_Processeur IS
+END test_Processeur;
+ 
+ARCHITECTURE behavior OF test_Processeur IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT Processeur
+    PORT(
+         Clk : IN  std_logic;
+         RST : IN  std_logic;
+         SA : OUT  std_logic_vector(7 downto 0);
+         SB : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal Clk : std_logic := '0';
+   signal RST : std_logic := '0';
+
+ 	--Outputs
+   signal SA : std_logic_vector(7 downto 0);
+   signal SB : std_logic_vector(7 downto 0);
+
+   -- Clock period definitions
+   constant Clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: Processeur PORT MAP (
+          Clk => Clk,
+          RST => RST,
+          SA => SA,
+          SB => SB
+        );
+
+   -- Clock process definitions
+   Clk_process :process
+   begin
+		Clk <= '0';
+		wait for Clk_period/2;
+		Clk <= '1';
+		wait for Clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for Clk_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
